650d65461978282b0ab925a36b4ad18b5c6b3ab2
Kohei Ikeda <tdn.suama@gmail.com>
2023-05-26 15:29:06 +0900

    Add vercel analytics.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics';
+
 import './style.css'
 // Leafletのスタイル読み込み
 import 'leaflet/dist/leaflet.css'
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+inject();
